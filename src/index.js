1ed53ac32941eabbc2d1a685809c246bb99bd48a
hungcao1405 <hungnguyen14599@gmail.com>
2021-04-21 10:43:08 +0700

    routes controllers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const handlebars  = require('express-handlebars');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
@@ -10,25 +10,28 @@ const routes = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 app.use(morgan('combined'));
 //router
 
-app.engine('.hbs', handlebars({
-  extname: '.hbs'
-}));
+app.engine(
+    '.hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 routes(app);
 
-
-
 //127.0.0.1 - localhost
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,20 +9,18 @@ function routes(app) {
     // app.get('/', (req, res) => {
     // return res.render('home');
     // })
-    
+
     // app.get('/news', (req, res) => {
     // return res.render('news');
     // })
-    
+
     // app.get('/search', (req, res) => {
     // return res.render('search');
     // })
-    
+
     // app.post('/search', (req, res) => {
     // console.log(req.body)
     // return res.send('')
     // })
-
-
 }
 module.exports = routes;
